3e4a531d304eb048d1a4d8a131fb383b946eb2ed
Jiacheng Zhang <jiachengzhang1@email.arizona.edu>
2018-10-09 00:28:14 -0700

    0.2

--- app/src/main/java/com/csc436/jz/sportsgroupup/Signup_1Page.java
+++ app/src/main/java/com/csc436/jz/sportsgroupup/Signup_1Page.java
@@ -1,50 +1,108 @@
 package com.csc436.jz.sportsgroupup;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Signup_1Page extends AppCompatActivity {
+    String Email, Password, PasswordReendter, Name;
+    public final static String EMAIL = "com.csc436.jz.sportsgroupup.EMAIL";
+    public final static String PASSWORD = "com.csc436.jz.sportsgroupup.PASSWORD";
+    public final static String NAME = "com.csc436.jz.sportsgroupup.NAME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_1_page);
 
-        Button signUpNext = findViewById(R.id.signUpNext);
-        Button signUpBack = findViewById(R.id.signUpBack);
+        Button signUpNext = findViewById(R.id.signup_Next);
+        Button signUpBack = findViewById(R.id.signup_back);
 
-        signUpNext.setOnClickListener(new View.OnClickListener(){
+
+        signUpNext.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
 
-                EditText email = findViewById(R.id.editText);
-                EditText password = findViewById(R.id.editText2);
-                EditText passwordReenter = findViewById(R.id.editText4);
-                EditText name = findViewById(R.id.editText5);
+                EditText email = (EditText) findViewById(R.id.signup_email);
+                EditText password = (EditText) findViewById(R.id.signup_password_1);
+                EditText passwordReenter = (EditText) findViewById(R.id.signup_password_2);
+                EditText name = (EditText) findViewById(R.id.signup_name);
 
-                Intent startIntent = new Intent(getApplicationContext(), Signup_2Page.class);
-                startActivity(startIntent);
+                Email = email.getText().toString();
+                Password = password.getText().toString();
+                PasswordReendter = passwordReenter.getText().toString();
+                Name = name.getText().toString();
+
+                if (Password.compareTo(PasswordReendter) != 0) {
+
+                } else {
+                    //Intent startIntent = new Intent(getApplicationContext(), Signup_2Page.class);
+                    //startActivity(startIntent);
+                    Intent intent = new Intent(Signup_1Page.this, Signup_2Page.class);
+                    intent.putExtra(EMAIL, Email);
+                    intent.putExtra(PASSWORD, Password);
+                    intent.putExtra(NAME, Name);
+                    startActivity(intent);
+                }
             }
-        } );
+        });
 
-        signUpBack.setOnClickListener(new View.OnClickListener(){
+        signUpBack.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View v){
-
-                EditText email = findViewById(R.id.editText);
-                EditText password = findViewById(R.id.editText2);
-                EditText passwordReenter = findViewById(R.id.editText4);
-                EditText name = findViewById(R.id.editText5);
+            public void onClick(View v) {
 
                 Intent startIntent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(startIntent);
             }
-        } );
+        });
+
     }
+
+    String path = "";
+
+    {
+        try {
+            URL url = new URL(path);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setConnectTimeout(5000);
+
+            connection.setRequestMethod("POST");
+
+            String data = "email=" + Email + "&password=" + Password + "&name=" + Name;
+            connection.setRequestProperty("Content_Tpye", "application/");
+
+            connection.setDoOutput(true);
+            OutputStream outputStream = connection.getOutputStream();
+            outputStream.write(data.getBytes());
+
+            int responseCode = connection.getResponseCode();
+      //      if(responseCode == 200) {
+       //         InputStream inputStream = connection.getInputStream();
+        //
+         //   } else {
+          //      return null;
+           // }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
--- app/src/main/java/com/csc436/jz/sportsgroupup/Signup_2Page.java
+++ app/src/main/java/com/csc436/jz/sportsgroupup/Signup_2Page.java
@@ -5,28 +5,51 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class Signup_2Page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        final Intent page1_intent = getIntent();
+      //  String email = intent.getStringExtra(Signup_1Page.EMAIL);
+      //  String password = intent.getStringExtra(Signup_1Page.PASSWORD);
+      //  String name = intent.getStringExtra(Signup_1Page.NAME);
+
+     //   System.out.println(email + " " + password+" "+name);
+
         setContentView(R.layout.activity_signup_2_page);
 
-        Button signUp1Next = findViewById(R.id.signUp1Next);
-        Button signUp1Back = findViewById(R.id.signUp1Back);
+        Button signUp2Next = findViewById(R.id.signup_next);
+        Button signUp2Back = findViewById(R.id.signup_back);
+        //signUp2Back.setText(page1_intent.getStringExtra(Signup_1Page.EMAIL));
 
-        signUp1Next.setOnClickListener(new View.OnClickListener(){
+        final RadioGroup radioGroup = (RadioGroup) findViewById(R.id.signup_radiogroup);
+
+
+        signUp2Next.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v){
 
-                Intent startIntent = new Intent(getApplicationContext(), Signup_3Page.class);
-                startActivity(startIntent);
+
+                int selectedID = radioGroup.getCheckedRadioButtonId();
+                if (selectedID != -1) {
+                    String text = ((RadioButton) findViewById(selectedID)).getText().toString();
+                } else {
+
+                }
+
+                Intent intent = new Intent(Signup_2Page.this, Signup_3Page.class);
+                intent.putExtras(page1_intent.getExtras());
+                startActivity(intent);
             }
         } );
 
-        signUp1Back.setOnClickListener(new View.OnClickListener(){
+        signUp2Back.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v){
