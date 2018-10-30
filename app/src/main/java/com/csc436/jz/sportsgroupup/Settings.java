835de557dadeb0ea799785f3b09f3f7c79db6615
aadamkassam <aadamkassam@email.arizona.edu>
2018-10-30 12:50:44 -0700

    settings page updated

--- app/src/main/java/com/csc436/jz/sportsgroupup/Settings.java
+++ app/src/main/java/com/csc436/jz/sportsgroupup/Settings.java
@@ -1,7 +1,11 @@
 package com.csc436.jz.sportsgroupup;
 
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Settings extends AppCompatActivity {
 
@@ -9,5 +13,38 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Button red = findViewById(R.id.redColor);
+        Button blue = findViewById(R.id.blueColor);
+        Button white = findViewById(R.id.defaultColor);
+        final ConstraintLayout settingsLayout = findViewById(R.id.settingsLayout);
+
+
+        red.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v){
+
+                settingsLayout.setBackgroundColor(Color.RED);
+            }
+        });
+
+        white.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v){
+
+                settingsLayout.setBackgroundColor(Color.WHITE);
+            }
+        });
+
+        blue.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v){
+
+                settingsLayout.setBackgroundColor(Color.BLUE);
+            }
+        });
     }
 }
