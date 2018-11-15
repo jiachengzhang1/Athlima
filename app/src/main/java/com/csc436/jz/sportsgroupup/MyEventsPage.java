d012c873e442ad41aaadce4a8f6a55b9c0c854f0
jiachengzhang1 <jiachengzhang1@email.arizona.edu>
2018-11-15 13:24:12 -0700

    need user interface for attendees and attending events, data is carried by ArrayList

--- app/src/main/java/com/csc436/jz/sportsgroupup/MyEventsPage.java
+++ app/src/main/java/com/csc436/jz/sportsgroupup/MyEventsPage.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.csc436.jz.sportsgroupup.Tasks.GetComingEventsTask;
 import com.csc436.jz.sportsgroupup.Tools.CurrentUser;
@@ -16,13 +17,20 @@ public class MyEventsPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_events_page);
 
+        TextView textView = new TextView(getApplicationContext());
+        ScrollView scrollView = findViewById(R.id.scrollView_goingEvents);
+
         Intent signin_intent = getIntent();
         CurrentUser currentUser = (CurrentUser) signin_intent.getSerializableExtra(MainPage.USER);
 
-        ScrollView scrollView = findViewById(R.id.scrollView_goingEvents);
         String url = URL.Address.url + ":3000/get/";
-        url = String.format(url + "searchUser=%s", currentUser.getCurrentUserEmail());
-        new GetComingEventsTask(getApplicationContext()).execute(url);
+        url = String.format(url + "/userEvent=%s", currentUser.getCurrentUserEmail());
+
+        /* connecting to server by calling GetComingEventTask
+        *  you can pass variables from this class to the task by add things into constructor
+        *  and in GetComingEventTask, context = getApplicationContext(); scrollview is useless now
+        * */
+        new GetComingEventsTask(getApplicationContext(),scrollView).execute(url);
 
     }
 }
--- app/src/main/java/com/csc436/jz/sportsgroupup/Tasks/GetComingEventsTask.java
+++ app/src/main/java/com/csc436/jz/sportsgroupup/Tasks/GetComingEventsTask.java
@@ -6,24 +6,32 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GetComingEventsTask extends AsyncTask<String, String, String> {
 
     @SuppressLint("StaticFieldLeak")
     private Context context;
+    private ScrollView scrollView;
 
-    public GetComingEventsTask (Context context) {
+    public GetComingEventsTask (Context context, ScrollView scrollView) {
+
         this.context = context;
+        this.scrollView = scrollView;
     }
 
     @TargetApi(Build.VERSION_CODES.KITKAT)
@@ -69,8 +77,33 @@ public class GetComingEventsTask extends AsyncTask<String, String, String> {
     protected void onPostExecute(String result) {
 
         JSONArray myJSONArray = null;
+        ArrayList<Map<String, String>> eventList = new ArrayList<>();
+
         try {
             myJSONArray = new JSONArray(result);
+            for(int i = 0; i<myJSONArray.length(); i++) {
+               // JSONObject object = new JSONObject(myJSONArray.get(i).toString());
+                JSONObject object = new JSONObject(myJSONArray.get(i).toString());
+
+                // a map for each event
+                Map<String, String> event = new HashMap<>();
+                event.put("id", object.getString("id"));
+                event.put("title", object.getString("name"));
+                event.put("date", object.getString("date"));
+                event.put("time", object.getString("time"));
+                event.put("location", object.getString("location"));
+                event.put("skill", object.getString("skill"));
+                event.put("description", object.getString("description"));
+                event.put("teamSize", object.getString("teamSize"));
+
+                // add event map into eventList
+                eventList.add(event);
+
+            }
+
+            // eventList has a list of event, the info of each event are carried by a map
+
+            Toast.makeText(context, eventList.toString(), Toast.LENGTH_LONG).show();
 
 
         } catch (JSONException e) {
